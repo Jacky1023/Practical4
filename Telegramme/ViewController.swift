5eff78e1766dda8ecea5bc4aa168ef2888e6fa70
Tan Yong Lun <S10187990@connect.np.edu.sg>
2019-11-04 17:17:33 +0800

    done with login page

--- Telegramme/ViewController.swift
+++ Telegramme/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var usrNameFld: UITextField!
+    
+    @IBOutlet weak var pwdFld: UITextField!
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
