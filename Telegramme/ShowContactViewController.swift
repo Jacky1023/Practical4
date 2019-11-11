06bd2df761c21ae70acb17573073b44a76f51087
irfan syahmi <s10189620@connect.np.edu.sg>
2019-11-11 11:54:59 +0800

    ContactList doesn't show anything

--- Telegramme/ShowContactViewController.swift
+++ Telegramme/ShowContactViewController.swift
@@ -10,7 +10,39 @@ import Foundation
 import UIKit
 
 class ShowContactViewController : UITableViewController{
+    
+    var appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.tableView.reloadData() //refresh data
     }
+    
+    override func viewDidAppear(_ animated: Bool){
+        self.tableView.reloadData()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView:UITableView, numberOfRowsInSection section:Int)->Int{
+        return appDelegate.contactList.count
+    }
+    
+    override func tableView(_ tableView:UITableView, cellForRowAt indexPath: IndexPath)->UITableViewCell{
+        
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "ContactCell", for: indexPath)
+        
+        let contact = appDelegate.contactList[indexPath.row]
+        cell.textLabel!.text = "\(contact.firstName) \(contact.lastName)"
+        cell.detailTextLabel!.text = "\(contact.mobileNo)"
+        
+        return cell
+    }
+    
+    
 }
+
+
